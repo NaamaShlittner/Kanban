597b3bea2fabcc070e9bfd379cea7d7e822ec3af
Naama <shlittna@post.bgu.ac.il>
2024-06-03 13:33:08 +0300

    added userservice and usersl

---
+++ ServiceLayer/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    internal class UserService
+    {
+        private UserFacade uf;
+
+        public Response SignUp(string email, string password);
+
+        public Response SignIn(string email, string password);
+    }
+}
