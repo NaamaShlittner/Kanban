bc21f504b670a37e1cfa1942f8d3f3ee81c3f6f2
Naama <shlittna@post.bgu.ac.il>
2024-06-03 13:18:58 +0300

    first commit

---
+++ ServiceLayer/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public class Response
+    {
+        public Object ResponseVal;
+        public string ErrorMessage;
+    }
+}
