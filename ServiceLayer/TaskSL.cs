c6bc79e6aebec0ed1824f742b9a5654bb24966c4
Nadav Svilim <nadavsvi@post.bgu.ac.il>
2024-06-03 13:55:06 +0300

    added task & board classes & boardServices

---
+++ ServiceLayer/TaskSL.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+	public class TaskSL
+	{
+        public long id;
+        public DateTime created;
+        public DateTime dueDate;
+        public string title;
+        public string desc;
+	}
+}
